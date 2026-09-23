ee359fd101ee59d62da55bcc2a4a33f01058d689
sebasmillan339-stack <sebasmillan339@gmail.com>
2026-09-22 23:38:48 -0500

    registro de un estudiante, calculo de definitiva y aprobado/reprobado

---
+++ src/main/java/Logica/Calculadora.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+    private String id;
+    private String nombre;
+    private double notaDesarrollo;
+    private double notaMatematica;
+    private double definitiva;
+
+    public Calculadora(String id, String nombre, double notaDesarrollo, double notaMatematica) {
+        this.id = id;
+        this.nombre = nombre;
+        this.notaDesarrollo = notaDesarrollo;
+        this.notaMatematica = notaMatematica;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public double getNotaDesarrollo() { return notaDesarrollo; }
+    public void setNotaDesarrollo(double notaDesarrollo) { this.notaDesarrollo = notaDesarrollo; }
+
+    public double getNotaMatematica() { return notaMatematica; }
+    public void setNotaMatematica(double notaMatematica) { this.notaMatematica = notaMatematica; }
+
+    public double getDefinitiva() { return definitiva; }
+
+    public double calcularDefinitiva(){
+        definitiva = notaMatematica * 0.4 + notaDesarrollo * 0.6;
+        return definitiva;
+    }
+
+    public void mostrarNota(){
+        JOptionPane.showMessageDialog(null, "Nombre: " + nombre + "\nCodigo: " + id + "\nNota definitiva: " + definitiva);
+    }
+}
