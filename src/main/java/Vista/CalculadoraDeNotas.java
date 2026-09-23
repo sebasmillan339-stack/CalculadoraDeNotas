134d98533e925e971f2809d7918df98acaf0c7b1
sebasmillan339-stack <sebasmillan339@gmail.com>
2026-09-23 00:13:01 -0500

    registro de un estudiante, calculo de definitiva y  aprobado/reprobado

--- src/main/java/Vista/CalculadoraDeNotas.java
+++ src/main/java/Vista/CalculadoraDeNotas.java
@@ -9,27 +9,19 @@ import javax.swing.JOptionPane;
 public class CalculadoraDeNotas {
 
     public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("Ingrese su Nombre");
+        String id = JOptionPane.showInputDialog("Ingrese su id");
+        double notaDesarrollo = Double.parseDouble(JOptionPane.showInputDialog("digite la nota de Desarrollo"));
+        double notaMatematica = Double.parseDouble(JOptionPane.showInputDialog("digite la nota de Matemáticas"));
 
-        int numeroEstudiantes = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de estudiantes: "));
-        Calculadora[] estudiantes = new Calculadora[numeroEstudiantes];
+        Calculadora estudiante = new Calculadora(id, nombre, notaDesarrollo, notaMatematica);
+        estudiante.calcularDefinitiva();
+        estudiante.mostrarNota();
 
-        for (int i = 0; i < numeroEstudiantes; i++) {
-            String nombre = JOptionPane.showInputDialog("Estudiante " + (i + 1) + "\nIngrese su Nombre");
-            String id = JOptionPane.showInputDialog("Estudiante " + (i + 1) + "\nIngrese su id");
-            double notaDesarrollo = Double.parseDouble(JOptionPane.showInputDialog("digite la nota de Desarrollo"));
-            double notaMatematica = Double.parseDouble(JOptionPane.showInputDialog("digite la nota de Matemáticas"));
-
-            estudiantes[i] = new Calculadora(id, nombre, notaDesarrollo, notaMatematica);
-            estudiantes[i].calcularDefinitiva();
-        }
-
-        for (int i = 0; i < numeroEstudiantes; i++) {
-            estudiantes[i].mostrarNota();
-            if (estudiantes[i].getDefinitiva() < 3.5) {
-                JOptionPane.showMessageDialog(null, "REPROBADO");
-            } else {
-                JOptionPane.showMessageDialog(null, "APROBADO");
-            }
+        if (estudiante.getDefinitiva() < 3.5) {
+            JOptionPane.showMessageDialog(null, "REPROBADO");
+        } else {
+            JOptionPane.showMessageDialog(null, "APROBADO");
         }
     }
 }
